7c997ae9834afd3aeb99d941156997f5f155da0d
Dec-F <dec_fan@icloud.com>
2020-04-08 15:03:17 +0800

    feat: try

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-function foo(bar: string): void {
-  console.log(bar);
-}
-foo("");
-export default foo;
+import Apify from "apify";
+import cheerio from "cheerio";
+
+Apify.main(async () => {
+  const requestQueue = await Apify.openRequestQueue();
+  await requestQueue.addRequest({ url: "https://netflav.com/" });
+  const pseudoUrls = [new Apify.PseudoUrl("https://netflav.com/[.*]")];
+  const crawler = new Apify.CheerioCrawler({
+    requestQueue,
+    handlePageFunction: async ({ $, response, request, body }) => {
+      const title = $("title");
+      const src = $("video").attr("src");
+      console.log(`Title of ${request.url}: ${title}`);
+      console.log(`Video of ${request.url}: ${src}`);
+      await Apify.utils.enqueueLinks({
+        $: cheerio.load(body),
+        selector: "a",
+        pseudoUrls,
+        baseUrl: "https://netflav.com",
+        requestQueue,
+      });
+    },
+    maxRequestsPerCrawl: 100,
+    maxConcurrency: 10,
+    proxyUrls: ["http://127.0.0.1:7890"],
+  });
+
+  await crawler.run();
+});
