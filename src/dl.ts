e47fa3bfc29d74a8d2d63ebd5717acdb2bd4f67b
Dec-F <dec_fan@icloud.com>
2020-04-09 16:40:42 +0800

    tag

--- src/dl.ts
+++ src/dl.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import axios from 'axios'
 
 export default async function downloadImage () {  
-  const url = 'https://fvs.io/redirector?token=c1BHVFd3V0VaRldIN1pISGFyRWZPMmkraC9Kd2MySlBvQzNGZk00QXp0NW8rS2h4Sm1kUXFQK05RL0hoZ2dhVWdsTE0vdkRQQUU1ZnRBY2dxYWFocW9LelJzbDRlcXI0L3Q0eW00dmxHeVpSS1hCYVN2RFdGcHYxbm1BVFZ5eForSTQzcUtLWDVUODVFMDU0enl0bFBkc2V0bzhsUlcyWHVBPT06Y2FWMFl0aXY2UlZFejBVL1piZEQ2UT09'
+  const url = 'http://yun.dui88.com/layer-site/video/7y43mt9fip_1586418340.ts'
   const fpath = path.resolve(__dirname, '../images', 'code.mp4')
   const writer = fs.createWriteStream(fpath)
 
@@ -11,17 +11,13 @@ export default async function downloadImage () {
     url,
     method: 'GET',
     headers:{
-        Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
-    }
-  }).then(res=>{
-      console.log(res);
-      return res
+    },
+    responseType: 'stream'
   }).catch(err=>{
-      console.log(err);
-      return err
+    console.log(err);
+    return err
   })
-
   response.data.pipe(writer)
 
   return new Promise((resolve, reject) => {
