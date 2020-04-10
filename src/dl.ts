11800056e3a64d58ced7b2479f8ec1b5cfe08f2c
Dec-F <dec_fan@icloud.com>
2020-04-10 21:07:07 +0800

    tag

--- src/dl.ts
+++ src/dl.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import axios from 'axios'
 
 export default async function downloadImage () {  
-  const url = 'http://yun.dui88.com/layer-site/video/7y43mt9fip_1586418340.ts'
+  const url = 'https://fvs.io/redirector?token=bXJWQXVZWFdLM253cE9DLzBVUzNOYlpOblh1eC9XWW0rZGhaeHlWN0h6cFAyR3NFNTArczlORTAyeU9YbnVlMG5jR0lCUEtibko3dDFId3daekdIVzVaWW5CcmMydFBld2w4WWFFVGJsTU5xU3BhbGR3bHVUdHNLOHBtMWRJeStmR2RiL3VFdUFjR0twdkx3SHkxemNOOVRzWjFzdy9hbTlSRT06YitMUm1rdHg2S0h5VS96ZmZWdWc0QT09'
   const fpath = path.resolve(__dirname, '../images', 'code.mp4')
   const writer = fs.createWriteStream(fpath)
 
@@ -11,7 +11,14 @@ export default async function downloadImage () {
     url,
     method: 'GET',
     headers:{
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
+        'User-Agent':'Mozilla/5.0 (compatible;Baiduspider-render/2.0; +http://www.baidu.com/search/spider.html)',
+        cookie:'__cfduid=dc1be798980bdc28c0a3aa35395826a9a1564201974',
+        referer:'https://www.avple.video/v/60g3et0e7zxj16l',
+        nobody:1,
+        header:1,
+        'accept-encoding':'gzip, deflate, br',
+        'sec-fetch-dest':'document',
+        'sec-fetch-mode': 'navigate'
     },
     responseType: 'stream'
   }).catch(err=>{
